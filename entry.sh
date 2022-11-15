32fe253df6188686691f12313efb205ad8a037e8
tigattack <10629864+tigattack@users.noreply.github.com>
2022-11-15 12:35:07 +0000

    entrypoint: refactor default values

--- entry.sh
+++ entry.sh
@@ -1,13 +1,21 @@
 #!/bin/sh -e
 
+# Set default values
+DEFAULT_CELERY_REDIS_HOST="redis"
+DEFAULT_CELERY_REDIS_PORT="6379"
+DEFAULT_GUNICORN_WORKERS="2"
+
 if [ "$1" = "worker" ]; then
+
+  # Set celery host & port to defaults if unset
   if [ -z "$CELERY_REDIS_HOST" ]; then
-    export CELERY_REDIS_HOST="redis"
+    export CELERY_REDIS_HOST="$DEFAULT_CELERY_REDIS_HOST"
   fi
   if [ -z "$CELERY_REDIS_PORT" ]; then
-    export CELERY_REDIS_PORT="6379"
+    export CELERY_REDIS_PORT="$DEFAULT_CELERY_REDIS_PORT"
   fi
 
+  # Run celery
   C_FORCE_ROOT=1 celery -b redis://${CELERY_REDIS_HOST}:${CELERY_REDIS_PORT}/0 -A patchman worker -l INFO -E
 
 elif [ "$1" = "server" ]; then
@@ -21,11 +29,12 @@ elif [ "$1" = "server" ]; then
   # This exists because there's no '--noinput' flag in Django 2, which patchman is based on.
   echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='$ADMIN_EMAIL', is_superuser=True).delete(); User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')" | ./manage.py shell
 
-  # Default number of workers if unspecified
+  # Set number of workers to default if unset
   if [ -z "$GUNICORN_WORKERS" ]; then
-    GUNICORN_WORKERS=2
+    GUNICORN_WORKERS="$DEFAULT_GUNICORN_WORKERS"
   fi
 
+  # Run gunicorn for patchman
   gunicorn --bind 0.0.0.0:80 --workers "$GUNICORN_WORKERS" patchman.wsgi:application
 
 else
